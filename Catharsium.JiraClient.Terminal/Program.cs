e0e657dd2d23c0c1c584e51b7e29f83b20a606f0
Thierry Brachthuizer <thierry.brachthuizer@unifiedpost.com>
2021-01-15 10:54:38 +0100

    Add project files.

---
+++ Catharsium.JiraClient.Terminal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Catharsium.JiraClient.Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
